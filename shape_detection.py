c1bfab79184b20a4274701fd16a7d7577a49154c
Jakob Hærvig <10219471+jakobhaervig@users.noreply.github.com>
2023-08-07 10:12:59 +0200

    Create shape_detection.py

---
+++ shape_detection.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+
+def image_rotate(image_path, degrees):
+    # Get a list of files in the directory
+    files = os.listdir(directory)
+
+    # Filter the list to include only image files (you can modify the condition as per your file extensions)
+    image_files = [file for file in files if file.endswith((".png", ".jpg", ".jpeg"))]
+
+    if not image_files:
+        print("No image files found in the directory.")
+        return
+
+    # Sort the image files based on their creation time (most recent first)
+    sorted_files = sorted(image_files, key=lambda file: os.path.getmtime(os.path.join(directory, file)), reverse=True)
+
+    # Select the newest image file
+    newest_image_path = os.path.join(directory, sorted_files[0])
+
+    # Rotate the newest image
+    image = Image.open(newest_image_path)
+    rotated_image = image.rotate(degrees, expand=True)
+    rotated_image.save("rotated_image.png")
+    rotated_image.show()
+    return image_path_rotated
+
+def image_canny(image_path, canny_value):
+
+    return image_path_canny
+
+
+def show_image(image_path):
+    pass
