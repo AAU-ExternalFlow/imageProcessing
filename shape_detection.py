331b9a7ea944ac2ff66b1cfbcf3e09d543c10af6
ScooterHenning <victorhm2000@gmail.com>
2023-08-09 13:30:06 +0200

    Update shape_detection.py

--- shape_detection.py
+++ shape_detection.py
@@ -2,10 +2,11 @@ import os
 from PIL import Image
 
 def image_rotate(image_path, degrees):
+
     image=Image.open(image_path)
     rotated_image = image.rotate(degrees, expand=True)
     rotated_image.save("uploads/blurred_image.png")
-    # rotated_image.show()
+    rotated_image.show()
     return image_path_rotated
 
 # def image_canny(image_path, canny_value):
