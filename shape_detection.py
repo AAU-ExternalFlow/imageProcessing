eefa4c8191d4c07eb4dca35837910ba048d9a856
ScooterHenning <victorhm2000@gmail.com>
2023-08-10 14:28:10 +0200

    Update shape_detection.py

--- shape_detection.py
+++ shape_detection.py
@@ -5,8 +5,8 @@ from PIL import Image
 def gaussian_blur(image, value):
     return cv2.GaussianBlur(image, (value, value), 0)
 
-# def canny(image, min_value, max_value):
-#     return cv2.Canny(image, min_value, max_value)
+def canny(image, min_value, max_value):
+    return cv2.Canny(image, min_value, max_value)
 
 # def bitwise_not(image):
 #     return cv2.bitwise_not(image)
